2aad7568c1233bafc6a903adf9565f75035f31c4
ZiyangTian <tianziyang@buaa.edu.cn>
2020-02-03 17:21:15 +0800

    v1

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,6 @@ from utils.os import fresh_dir
 from utils.os import files_in_dir
 from utils.os import require_dir
 from utils.os import make_dir
+from utils.typing import attrdict_to_json
+from utils.typing import attrdict_from_json
+from utils.typing import normalize_list_of_type
