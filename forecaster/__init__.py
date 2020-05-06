52d2e4ea55c9efc14faa3cfb57cf8d07756c09d5
ZiyangTian <tianziyang@buaa.edu.cn>
2020-05-06 15:56:33 +0800

    standalone

--- forecaster/__init__.py
+++ forecaster/__init__.py
@@ -2,6 +2,12 @@
 from forecaster.version import __version__
 
 from forecaster import apps
+from forecaster import data
+from forecaster.data.sequence import SeqColumnsSpec
+from forecaster.data.sequence import ReducingColumnsSpec
+from forecaster.data.sequence import RawDataSpec
+from forecaster.data.sequence import sequence_dataset
+from forecaster.data.sequence import multi_sequence_dataset
 from forecaster.apps.base import get_app
 from forecaster.apps.base import app
 from forecaster.apps.base import App
